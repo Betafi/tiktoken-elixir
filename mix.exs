2bc0d8cd830b722808bba58fc2690fd6c99f49ff
Woo Jia Hao <woojiahao1234@gmail.com>
2023-07-10 17:18:16 +0800

    Fix package

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Tiktoken.MixProject do
 
   defp package do
     [
-      files: ~w(lib priv .formatter.exs mix.exs README* LICENSE* native checksum-*.exs),
+      files: ~w(lib mix.exs README* LICENSE* native checksum-*.exs),
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/connorjacobsen/tiktoken-elixir"}
     ]
